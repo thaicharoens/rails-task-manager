ac315454f7444138dd764c5f01da1c39d0fe380f
Sunny Thaicharoen <thaicharoens@me.com>
2019-07-23 18:56:28 +0100

    Rails commit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,13 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def add
+    @new_task = Task.new
+  end
 end
